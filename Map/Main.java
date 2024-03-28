8f887470dd3c195010b00a15637d84d1046f76d6
Peter <xsterfo456@gmail.com>
2024-03-28 22:09:43 +0800

    third commit

--- Map/Main.java
+++ Map/Main.java
@@ -15,5 +15,21 @@ public class Main {
         System.out.println("Removed value of KEY1: " + myMap.remove("KEY1"));
         System.out.println("Removed value of KEY3: " + myMap.remove("KEY3"));
         System.out.println("Removed value of KEY4: " + myMap.remove("KEY4"));
+
+        System.out.println();
+
+        MyMap<String, Double> myMap2 = new MyMap<>();
+
+        myMap2.put("KEY1", 20.51);
+        myMap2.put("KEY2", 30.15);
+        myMap2.put("KEY3", 100.12);
+
+        System.out.println("Value of KEY1: " + myMap2.get("KEY1"));
+        System.out.println("Value of KEY2: " + myMap2.get("KEY3"));
+        System.out.println("Value of KEY4: " + myMap2.get("KEY4"));
+
+        System.out.println("Removed value of KEY1: " + myMap2.remove("KEY1"));
+        System.out.println("Removed value of KEY3: " + myMap2.remove("KEY3"));
+        System.out.println("Removed value of KEY4: " + myMap2.remove("KEY4"));
     }
 }
